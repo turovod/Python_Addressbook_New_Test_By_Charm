ad42baec42154152c4cf7a3f487040e0b595182a
Sergey <turovodspb@gmail.com>
2020-12-08 22:30:34 +0300

    Extract methods

--- add_new_group.py
+++ add_new_group.py
@@ -15,16 +15,20 @@ class UntitledTestCase(unittest.TestCase):
 
     def test_untitled_test_case(self):
         driver = self.driver
-        driver.get("http://localhost/addressbook/")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_id("LoginForm").click()
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_home_page(driver)
+        self.login(driver)
+        self.open_groups_page(driver)
+        self.create_group(driver)
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+
+    def logout(self, driver):
+        driver.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, driver):
         driver.find_element_by_link_text("groups").click()
+
+    def create_group(self, driver):
         driver.find_element_by_name("new").click()
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
@@ -36,8 +40,22 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_name("group_footer").clear()
         driver.find_element_by_name("group_footer").send_keys("ccc")
         driver.find_element_by_name("submit").click()
+
+    def open_groups_page(self, driver):
         driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_link_text("Logout").click()
+
+    def login(self, driver):
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_id("LoginForm").click()
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try:
