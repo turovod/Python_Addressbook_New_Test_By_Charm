e06578c331f31b08107387e3bf06531f8ab90d80
Sergey <turovodspb@gmail.com>
2020-12-14 18:15:08 +0300

    Login and logout common for all tests

--- tests/add_new_group.py
+++ tests/add_new_group.py
@@ -3,6 +3,5 @@ from model.group import Group
 
 
 def test_add_new_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="aaa", header="bbb", footer="ccc"))
-    app.session.logout()
+
