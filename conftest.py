0a6c6e894d649ecd16722e5d955b3f310eb89f8a
Sergey <turovodspb@gmail.com>
2020-12-24 19:23:24 +0300

    Added console run tests

--- conftest.py
+++ conftest.py
@@ -6,15 +6,19 @@ fixture = None
 
 # @pytest.fixture(scope="session") Common fixture for all session
 @pytest.fixture
-def app():
+def app(request):
     global fixture
-
+    # data for console input
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture = Application()
+        # fixture = Application() # no console input
+        fixture = Application(browser=browser, base_url=base_url)
         # fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
-             fixture = Application()
+            # fixture = Application() # no console input
+            fixture = Application(browser=browser, base_url=base_url)
         # fixture.session.login(username="admin", password="secret")
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
@@ -30,3 +34,9 @@ def stop(request):
 
     request.addfinalizer(fin)
     return fixture
+
+
+# data for console input
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
--- fixture/application.py
+++ fixture/application.py
@@ -7,15 +7,25 @@ from fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self):
-        self.driver = webdriver.Firefox(executable_path="../Drivers/geckodriver.exe")
-        # self.driver = webdriver.Chrome(executable_path="../Drivers/chromedriver.exe")
-        # self.driver = webdriver.Ie(executable_path="../Drivers/iedriver.exe")
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            # launch from IDE
+            # self.driver = webdriver.Firefox(executable_path="../Drivers/geckodriver.exe")
+            # launch from console
+            self.driver = webdriver.Firefox(executable_path="D:/Pithon/Python_Addressbook_New_Test_By_Charm/Python_Addressbook_New_Test_By_Charm/Drivers/geckodriver.exe")
+        elif browser == "chrome":
+            self.driver = webdriver.Chrome(executable_path="D:/Pithon/Python_Addressbook_New_Test_By_Charm/Python_Addressbook_New_Test_By_Charm/Drivers/chromedriver.exe")
+        elif browser == "edge":
+            self.driver = webdriver.Edge(executable_path="D:/Pithon/Python_Addressbook_New_Test_By_Charm/Python_Addressbook_New_Test_By_Charm/Drivers/msedgedriver.exe")
+        else:
+            raise ValueError(f"Unrecognised browser {browser}")
         # self.driver.implicitly_wait(1) # waiting in seconds
-        self.base_url = "http://localhost/addressbook/"
+        # self.base_url = "http://localhost/addressbook/"
+        self.base_url = base_url
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
