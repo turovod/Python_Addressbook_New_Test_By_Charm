1d95ac5e950863a26b7f32d630326a09cf265280
Sergey <turovodspb@gmail.com>
2020-12-25 18:00:24 +0300

    Added config file json

--- conftest.py
+++ conftest.py
@@ -1,26 +1,27 @@
+import json
 import pytest
 from fixture.application import Application
+# warning! for the compiler to read the json file in the root directory it needs to be made working
 
 fixture = None
-
+target = None
 
 # @pytest.fixture(scope="session") Common fixture for all session
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     # data for console input
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    # base_url = request.config.getoption("--baseUrl") # base_url is config_file
+    if fixture is None or not fixture.is_valid():
         # fixture = Application() # no console input
-        fixture = Application(browser=browser, base_url=base_url)
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
         # fixture.session.login(username="admin", password="secret")
-    else:
-        if not fixture.is_valid():
-            # fixture = Application() # no console input
-            fixture = Application(browser=browser, base_url=base_url)
-        # fixture.session.login(username="admin", password="secret")
-    fixture.session.ensure_login(username="admin", password="secret")
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -39,4 +40,5 @@ def stop(request):
 # data for console input
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    # parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
